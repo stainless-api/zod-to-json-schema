329839698302eaab51dbfb2b298fccb51587fa83
Robert Craigie <robert@craigie.dev>
2023-10-30 11:14:35 +0000

    Try fix

--- copyPackageFiles.js
+++ copyPackageFiles.js
@@ -1,5 +1,9 @@
-const { copyFileSync } = require("fs");
+const { copyFileSync, readFileSync, writeFileSync } = require("fs");
 
 for (const file of ["README.md", "package.json", "LICENSE", "changelog.md"]) {
   copyFileSync(`./${file}`, `./dist/${file}`);
 }
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+pkg.main = "index.js";
+writeFileSync("./dist/package.json", JSON.stringify(pkg, null, 2), 'utf8');
